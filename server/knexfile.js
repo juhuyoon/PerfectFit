3fc05037ad656af23223fca840bb7f024a4eb166
Erwins Saget <erwins.saget@macys.com>
2020-06-29 04:34:57 -0400

    Add configurations for different database environments

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,64 +1,23 @@
-// Update with your config settings.
-
-const migrations = {
-  directory: './database/migrations',
-  tableName: 'knex_migrations',
-  stub: './database/migration.stub',
-};
-
-const seeds = {
-  directory: './database/seeds',
-};
+const config = require('config');
 
 module.exports = {
   test: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3',
-    },
-    migrations,
-    seeds,
+    client: config.get('knex.client'),
+    connection: config.get('knex.connection'),
   },
 
   development: {
-    client: 'postgresql',
-    connection: {
-      host: 'postgres',
-      user: 'postgres',
-      password: 'password',
-      database: 'perfectfitDB',
-    },
-    migrations,
-    seeds,
-    debug: true,
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations,
-    seeds,
+    client: config.get('knex.client'),
+    connection: config.get('knex.connection'),
+    pool: config.get('knex.pool'),
+    migrations: config.get('knex.migrations'),
+    seeds: config.get('knex.seeds'),
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations,
+    client: config.get('knex.client'),
+    connection: config.get('knex.connection'),
+    pool: config.get('knex.pool'),
+    migrations: config.get('knex.migrations'),
   },
 };
