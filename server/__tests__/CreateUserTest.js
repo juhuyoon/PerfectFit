899a21fcdb22f9e4bf9127845c53b31390ca09e6
Erwins Saget <erwinssaget@gmail.com>
2020-07-08 21:26:24 -0400

    Feature/Registration
    This feature adds the ability for user to register, hitting the /members route, validation added for this feature, and the models and migrations have also been added. 
    This commit also includes tests and adds Github Workflows.

---
+++ server/__tests__/CreateUserTest.js
@@ -0,0 +1,216 @@
+const request = require('supertest');
+const config = require('config');
+const faker = require('faker');
+const knexDbManager = require('knex-db-manager');
+const app = require('../src/app');
+
+const dbManager = knexDbManager.databaseManagerFactory({
+  knex: config.get('knex'),
+  dbManager: {
+    superUser: config.get('knex.connection.user'),
+    superPassword: config.get('knex.connection.password'),
+    populatePathPattern: 'database/seeds/**/*.js', // glob format for searching seeds
+  },
+});
+
+beforeAll(async () => {
+  const knex = await dbManager.knexInstance();
+
+  try {
+    await knex.migrate.latest();
+  } catch (err) {
+    // migrations already exist =)
+  }
+});
+
+beforeEach(async () => {
+  const ignoredTables = ['migrations'];
+
+  await dbManager.truncateDb(ignoredTables);
+});
+
+afterAll(() => {
+  // close db connections
+  dbManager.closeKnex();
+  dbManager.close();
+});
+
+async function makePostRequest(url, attributes = {}) {
+  if (!url) {
+    throw new Error(`No url was provided`);
+  }
+
+  const res = await request(app).post(url).send(attributes);
+
+  return { res };
+}
+
+describe('Creating a user', () => {
+  test('it creates a user given a perfect scenario', async () => {
+    const email = faker.internet.email();
+    const full_name = faker.name.findName();
+    const username = faker.internet.userName();
+
+    const { res } = await makePostRequest('/members', {
+      full_name,
+      username,
+      email,
+      password: 'password',
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty('email');
+    expect(res.body).toHaveProperty('full_name', full_name);
+    expect(res.body).toHaveProperty('username', username);
+    expect(res.body).not.toHaveProperty('password');
+  });
+
+  test('it returns an error if no name is given', async () => {
+    const email = faker.internet.email();
+    const username = faker.internet.userName();
+
+    const { res } = await makePostRequest('/members', {
+      username,
+      email,
+      password: 'password',
+    });
+
+    expect(res.statusCode).toBe(422);
+    expect(res.body).toHaveProperty('errors');
+    expect(res.body.errors).toContainEqual({
+      msg: 'Please provide your name.',
+      param: 'full_name',
+      location: 'body',
+    });
+  });
+
+  test('it returns an error if no password is given', async () => {
+    const email = faker.internet.email();
+    const username = faker.internet.userName();
+    const full_name = faker.name.findName();
+
+    const { res } = await makePostRequest('/members', {
+      username,
+      email,
+      full_name,
+    });
+
+    expect(res.statusCode).toBe(422);
+    expect(res.body).toHaveProperty('errors');
+    expect(res.body.errors).toContainEqual({
+      msg: 'Please provide a password that is at least 7 characters long',
+      param: 'password',
+      location: 'body',
+    });
+  });
+
+  test('it returns an error if password given is too short', async () => {
+    const email = faker.internet.email();
+    const username = faker.internet.userName();
+    const full_name = faker.name.findName();
+
+    const { res } = await makePostRequest('/members', {
+      username,
+      email,
+      full_name,
+      password: 'secret', // Only 6 characters
+    });
+
+    expect(res.statusCode).toBe(422);
+    expect(res.body).toHaveProperty('errors');
+    expect(res.body.errors).toContainEqual({
+      msg: 'Please provide a password that is at least 7 characters long',
+      param: 'password',
+      location: 'body',
+      value: 'secret',
+    });
+  });
+
+  test('it returns an error if no email is given', async () => {
+    const username = faker.internet.userName();
+    const full_name = faker.name.findName();
+
+    const { res } = await makePostRequest('/members', {
+      username,
+      full_name,
+      password: 'password',
+    });
+
+    expect(res.statusCode).toBe(422);
+    expect(res.body).toHaveProperty('errors');
+    expect(res.body.errors).toContainEqual({
+      msg: expect.any(String),
+      param: 'email',
+      location: 'body',
+    });
+  });
+
+  test('it returns an error if email is taken', async () => {
+    const email = faker.internet.email();
+
+    await makePostRequest('/members', {
+      email,
+      full_name: faker.name.findName(),
+      username: faker.internet.userName(),
+      password: 'password',
+    });
+
+    const { res } = await makePostRequest('/members', {
+      email,
+      full_name: faker.name.findName(),
+      username: faker.internet.userName(),
+      password: 'password',
+    });
+
+    expect(res.statusCode).toBe(422);
+    expect(res.body).toHaveProperty('errors');
+    expect(res.body.errors).toContainEqual({
+      msg: 'That E-mail address is already in use',
+      param: 'email',
+      location: 'body',
+      value: expect.any(String),
+    });
+  });
+
+  test('it returns an error if no username is given', async () => {
+    const { res } = await makePostRequest('/members', {
+      email: faker.internet.email(),
+      full_name: faker.name.findName(),
+      password: 'password',
+    });
+
+    expect(res.statusCode).toBe(422);
+    expect(res.body).toHaveProperty('errors');
+    expect(res.body.errors).toContainEqual({
+      msg: expect.any(String),
+      param: 'username',
+      location: 'body',
+    });
+  });
+
+  test('it returns an error if username is taken', async () => {
+    const username = faker.internet.userName();
+
+    await makePostRequest('/members', {
+      email: faker.internet.email(),
+      full_name: faker.name.findName(),
+      username,
+      password: 'password',
+    });
+    const { res } = await makePostRequest('/members', {
+      username,
+      email: faker.internet.email(),
+      full_name: faker.name.findName(),
+      password: 'password',
+    });
+
+    expect(res.statusCode).toBe(422);
+    expect(res.body).toHaveProperty('errors');
+    expect(res.body.errors).toContainEqual({
+      msg: expect.any(String),
+      param: 'username',
+      location: 'body',
+      value: expect.any(String),
+    });
+  });
+});
